af7e141bbb7f77a17cf208bcd0af4e6554af2403
timon-laptop <timon.schelling@gmail.com>
2019-11-20 09:18:41 +0100

    implement navigator

--- memory/arrayMemory.go
+++ memory/arrayMemory.go
@@ -1,24 +1,62 @@
 package memory
 
 type SimpleArrayMemory struct {
-	Backend []bool
+	backend []bool
 }
 
 func NewSimpleArrayMemory() SimpleArrayMemory {
-	return SimpleArrayMemory{Backend: []bool{}}
+	return SimpleArrayMemory{backend: []bool{}}
 }
 
 func (m SimpleArrayMemory) Read(address Address, length uint64) (r []Bit) {
 	for i := uint64(0); i < length; i++ {
-		r = append(r, Bit(m.Backend[i+uint64(address)]))
+		r = append(r, Bit(m.backend[i+uint64(address)]))
 	}
 	return r
 }
 
-func (SimpleArrayMemory) Write(address Address, value []Bit) {
-	panic("implement me")
+func (m SimpleArrayMemory) Write(address Address, value []Bit) {
+	valueLength := len(value)
+	for len(m.backend) < valueLength {
+		m.backend = append(m.backend, false)
+	}
+	for i := 0; i < valueLength; i++ {
+		currentAddress := Address(i) + address
+		if currentAddress == Address(len(m.backend)) {
+			m.backend = append(m.backend, bool(value[i]))
+		} else {
+			m.backend[currentAddress] = bool(value[i])
+		}
+	}
 }
 
 func (SimpleArrayMemory) Navigator() Navigator {
 	panic("implement me")
+}
+
+type SimpleArrayMemoryNavigator struct {
+	memory  SimpleArrayMemory
+	current Address
+}
+
+func (mn SimpleArrayMemoryNavigator) Jump(address Address) {
+	mn.current = address
+}
+
+func (mn SimpleArrayMemoryNavigator) Skip() {
+	mn.current++
+}
+
+func (mn SimpleArrayMemoryNavigator) Back() {
+	mn.current--
+}
+
+func (mn SimpleArrayMemoryNavigator) Next() Bit {
+	mn.memory.Read(mn.current, 1)
+	mn.current++
+}
+
+func (mn SimpleArrayMemoryNavigator) Last() Bit {
+	mn.current--
+	mn.memory.Read(mn.current, 1)
 }
