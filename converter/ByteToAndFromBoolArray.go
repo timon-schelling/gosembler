5e35784a76df1d1b6595afcac2da8a95f31494ed
timon-laptop <timon.schelling@gmail.com>
2019-11-28 11:06:00 +0100

    add byte to bool array utils

---
+++ converter/ByteToAndFromBoolArray.go
@@ -0,0 +1,23 @@
+package converters
+
+func boolsToBytes(t []bool) []byte {
+	b := make([]byte, (len(t)+7)/8)
+	for i, x := range t {
+		if x {
+			b[i/8] |= 0x80 >> uint(i%8)
+		}
+	}
+	return b
+}
+
+func bytesToBools(b []byte) []bool {
+	t := make([]bool, 8*len(b))
+	for i, x := range b {
+		for j := 0; j < 8; j++ {
+			if (x<<uint(j))&0x80 == 0x80 {
+				t[8*i+j] = true
+			}
+		}
+	}
+	return t
+}
